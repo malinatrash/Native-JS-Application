df2adeb50e4410066678b5ecba2810759576a26c
malinatrash <malinatrashh@gmail.com>
2023-01-31 14:18:24 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.[chunkhash].js',
+        path: path.resolve(__dirname, 'public')
+    },
+    devServer: {
+        port: 3000
+    },
+    plugins: [
+        new HTMLPlugin({
+            template: "./src/index.html"
+        }),
+        new CleanWebpackPlugin()
+    ]
+}
