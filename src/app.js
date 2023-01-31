c98e92ac3e2ff97025768c89187a672d31cdbe5f
malinatrash <malinatrashh@gmail.com>
2023-01-31 18:09:45 +0800

    Сделал фронтенд для приложения. Вопрос, введеный в поле выводится в консоль по нажатию кнопки и проходит проверку на валидацию.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
 import './static/style.css'
+import {isValid} from "./utils";
+
+const form = document.getElementById('form')
+const input = form.querySelector('#question-input')
+const sumbitButton = form.querySelector('#submit')
+
+form.addEventListener('submit', submitFormHandler)
+input.addEventListener('input', () => {
+    sumbitButton.disabled = !isValid(input.value)
+})
+
+function submitFormHandler(event) {
+    event.preventDefault()
+    if (isValid(input.value)) {
+        const question = {
+            text: input.value.trim(),
+            date: new Date().toJSON()
+
+        }
+        sumbitButton.disabled = true
+        // Async request to server to save question
+        console.log('Question: ', question);
+        input.value = ''
+        input.className = ''
+        sumbitButton.disabled = false
+    }
+}
---
+++ src/utils.js
@@ -0,0 +1,3 @@
+export const isValid = (value) => {
+    return value.length >= 10
+}
